7a170974d8481e1ba8a1caba1d3d6f41ac5075b7
Jayamangala Manmadha <manmadhajayamangala777@gmail.com>
2025-04-28 14:31:41 +0530

    Update urls.py

--- mainproject/urls.py
+++ mainproject/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+if settings.DEBUG:  # Only in development mode
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
